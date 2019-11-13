6af311fc34d46512d7124c2f9db64e0c40b40a6f
ilya <ilya@ilyas-MacBook-Air.local>
2019-11-13 20:20:43 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
+const Utils = artifacts.require("Utils");
 const Microcredit = artifacts.require("Microcredit");
 const ProxyMicrocredit = artifacts.require("ProxyMicrocredit");
 
 
 module.exports = (deployer) => {
+    deployer.deploy(Utils);
+    deployer.link(Utils, Microcredit);
     deployer.deploy(Microcredit);
     deployer.deploy(ProxyMicrocredit);
 };
